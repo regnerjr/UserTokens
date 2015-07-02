b09b51aac3290ef49fc98f8fd19d4810f1cd3bc5
John Regner <john@johnregner.com>
2015-07-02 14:12:02 -0500

    Added More Tests

--- UserTokens/UserTokensTests/UserTokensTests.swift
+++ UserTokens/UserTokensTests/UserTokensTests.swift
@@ -1,35 +1,66 @@
-//
-//  UserTokensTests.swift
-//  UserTokensTests
-//
-//  Created by John Regner on 6/30/15.
-//  Copyright © 2015 HumanAPI-JohnRegner. All rights reserved.
-//
-
 import XCTest
+@testable import UserTokens
 
 class UserTokensTests: XCTestCase {
-    
+
+    var ad: AppDelegate!
+    var window: UIWindow!
+    var rvc: UISplitViewController!
+    var masterNav: UINavigationController!
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        ad = UIApplication.sharedApplication().delegate as! AppDelegate
+        window = ad.window!
+        rvc = window.rootViewController as! UISplitViewController
+        masterNav = rvc.viewControllers[0] as! UINavigationController
+
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMasterVC() {
+        let master = masterNav.topViewController as! MasterViewController
+        XCTAssert(master.hasSelection == false)
+        let addButton = master.navigationItem.rightBarButtonItem!
+        master.insertNewObject(addButton)
+        XCTAssert(master.presentedViewController != nil)
+
+        let addUserVC = master.presentedViewController! as! NewUserViewController
+        addUserVC.emailTextField.text = "john@john.com"
+        addUserVC.textFieldShouldReturn(addUserVC.emailTextField)
+        addUserVC.passwordTextField.text = "asdfasdf123"
+        addUserVC.textFieldShouldReturn(addUserVC.passwordTextField)
+        XCTAssert(addUserVC.doneButton.enabled == true)
+
+        let button = UIBarButtonItem()
+        addUserVC.done(button)
+        XCTAssert(master.users.count > 0 )
+        master.tableView.reloadData()
+        //back to mainVC Select the first item
+        let indexPath = NSIndexPath(forItem: 0, inSection: 0)
+        master.tableView.selectRowAtIndexPath(indexPath, animated: false, scrollPosition: UITableViewScrollPosition.Top)
+        XCTAssert( master.hasSelection == true )
+
+        XCTAssert(master.users[0].userName == "john@john.com")
+        XCTAssert(master.users[5].userName == "john@john.com")
+        //falls back to returning the first item in the set
+
+        master.performSegueWithIdentifier("showDetail", sender: master)
+
+        //assert something here
+
     }
+
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measureBlock() {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+
 }
--- UserTokens/UserTokensUITests/UserTokensUITests.swift
+++ UserTokens/UserTokensUITests/UserTokensUITests.swift
@@ -21,9 +21,7 @@ class UserTokensUITests: XCTestCase {
     }
     
     func testAddUserButtonShowsUserSheet() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let app = XCUIApplication()
+
         let addButton = app.navigationBars["Users"].buttons["Add"]
         addButton.tap()
         app.toolbars.buttons["Cancel"].tap()
@@ -43,6 +41,7 @@ class UserTokensUITests: XCTestCase {
         //check that done is not enabled
         XCTAssertEqual(app.buttons["Done"].enabled, false)
 
+<<<<<<< HEAD
         password1TextField.doubleTap()
         let keyboard = app.keyboards.elementAtIndex(0)
 //        keyboard.typeKey(XCUIKeyboardKeyDelete, modifierFlags: XCUIKeyModifierFlags.None)
@@ -55,30 +54,59 @@ class UserTokensUITests: XCTestCase {
         app.childrenMatchingType(.Window).elementAtIndex(0)
             .childrenMatchingType(.Unknown).elementAtIndex(0)
             .childrenMatchingType(.Unknown).elementAtIndex(0).tap()
+=======
+        password1TextField.tap()
+        password1TextField.typeText("123")
+        app.keyboards.buttons["Return"].tap()
+
+>>>>>>> Added More Tests
         //check that done is enabled
         XCTAssertEqual(app.buttons["Done"].enabled, true)
 
         emailTextField.tap()
+<<<<<<< HEAD
         emailTextField.typeText("john") //not a valid email
         app.childrenMatchingType(.Window).elementAtIndex(0)
             .childrenMatchingType(.Unknown).elementAtIndex(0)
             .childrenMatchingType(.Unknown).elementAtIndex(0).tap()
+=======
+        emailTextField.tap()
+        app.menuItems["Select All"].tap()
+        app.menuItems["Cut"].tap()
+        emailTextField.typeText("john")
+        app.typeText("\n")
+
+        
+>>>>>>> Added More Tests
         //check that done button is disabled
         XCTAssertEqual(app.buttons["Done"].enabled, false)
 
         emailTextField.tap()
+        emailTextField.tap()
+        app.menuItems["Select All"].tap()
+        app.menuItems["Cut"].tap()
+
         emailTextField.typeText("john@john.com") //not a valid email
+<<<<<<< HEAD
         app.childrenMatchingType(.Window).elementAtIndex(0)
             .childrenMatchingType(.Unknown).elementAtIndex(0)
             .childrenMatchingType(.Unknown).elementAtIndex(0).tap()
+=======
+        app.keyboards.buttons["Return"].tap()
+>>>>>>> Added More Tests
 
         //click done button
         XCUIApplication().toolbars.buttons["Done"].tap()
 
         //check that john@john.com is now in master list
+<<<<<<< HEAD
         let list = app.tables.elementAtIndex(0)
         let name = list.cells.elementAtIndex(0).staticTexts.elementAtIndex(0)
 
+=======
+        XCUIApplication().tables.staticTexts["john@john.com"].tap()
+>>>>>>> Added More Tests
     }
-    
+
+
 }
