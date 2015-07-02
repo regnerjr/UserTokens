1cdd3e5c48aea08b8e4d1f60fff9b970ba8a5261
John Regner <john@johnregner.com>
2015-07-02 11:04:49 -0500

    Added rest of Edit user logic. Started adding UITests

--- UserTokens/UserTokens/EditUserViewController.swift
+++ UserTokens/UserTokens/EditUserViewController.swift
@@ -1,33 +1,99 @@
-//
-//  EditUserViewController.swift
-//  UserTokens
-//
-//  Created by John Regner on 7/1/15.
-//  Copyright © 2015 HumanAPI-JohnRegner. All rights reserved.
-//
-
 import UIKit
 
 class EditUserViewController: UIViewController {
 
-    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var idLabel: UILabel!
 
-    var user: TokensUser! { //initialize this before presenting this VC
-        willSet {
-            userNameLabel?.text = newValue.userName
-        }
-    }
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+
+    var user: TokensUser! // Must be configured in prepare for segue
+    var users: Set<TokensUser>! //must be configured in prepare for segue 
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        userNameLabel.text = user?.userName
+        configureView()
     }
 
-    @IBAction func done(sender: UIBarButtonItem) {
-        if let pres = presentingViewController as? DetailViewController {
-            pres.detailItem = user
+    func configureView() {
+        if user != nil {
+            userNameField.text = user.userName
+            passwordField.text = user.password
+            idLabel.text = user.id.UUIDString
         }
-        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    // MARK: - Actions
+    @IBAction func newID(sender: UIButton) {
+        let newID = NSUUID()
+        idLabel.text = newID.UUIDString
+        users.remove(user)
+        user = TokensUser(userName: user.userName, password: user.password, id: newID, tokens: user.tokens, lastUpdated: NSDate())
+        users.insert(user)
+    }
+    @IBAction func backgroundTapped(sender: UITapGestureRecognizer) {
+        if userNameField.isFirstResponder() {
+            checkForEmailConformance()
+            userNameField.resignFirstResponder()
+        }
+        if passwordField.isFirstResponder(){
+            checkForPasswordConformance()
+            passwordField.resignFirstResponder()
+        }
+    }
+}
+
+
+extension EditUserViewController: UITextFieldDelegate {
+
+    func checkForEmailConformance() -> Bool {
+        let validEmail = isUserNameValid(users, email: userNameField.text)
+        if validEmail == false {
+            //show popup about password requirements
+            let alert = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+            let OK = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            alert.title = "Usernames Validation Fail"
+            alert.message = "Must be Unique and contain '@' and '.' characters"
+            alert.addAction(OK)
+            presentViewController(alert, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
+
+    func checkForPasswordConformance() -> Bool{
+        let validPassword = isPasswordValid(passwordField.text)
+        if validPassword == false {
+            //show popup about requrements for usernames
+            let alert = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+            let OK = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            alert.addAction(OK)
+            alert.title = "Password Validation Fail"
+            alert.message = "Must be longer than 8 characters and contain a number"
+            presentViewController(alert, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
+
+    func checkForUserNameAndPasswordConformance() -> Bool{
+        return checkForPasswordConformance() && checkForEmailConformance()
+    }
+
+    func updateUser(){
+        users.remove(user)
+        user.lastUpdated = NSDate()
+        user.userName = userNameField.text!
+        user.password = passwordField.text!
+        users.insert(user)
+    }
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        if textField == passwordField && checkForPasswordConformance() {
+                updateUser()
+        } else if textField == userNameField && checkForEmailConformance() {
+                updateUser()
+        }
+    }
 }
--- UserTokens/UserTokensUITests/UserTokensUITests.swift
+++ UserTokens/UserTokensUITests/UserTokensUITests.swift
@@ -1,16 +1,8 @@
-//
-//  UserTokensUITests.swift
-//  UserTokensUITests
-//
-//  Created by John Regner on 6/30/15.
-//  Copyright © 2015 HumanAPI-JohnRegner. All rights reserved.
-//
-
 import Foundation
 import XCTest
 
 class UserTokensUITests: XCTestCase {
-        
+    var app: XCUIApplication! = nil
     override func setUp() {
         super.setUp()
         
@@ -19,7 +11,8 @@ class UserTokensUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app = XCUIApplication()
+        app.launch()
     }
     
     override func tearDown() {
@@ -27,9 +20,50 @@ class UserTokensUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testAddUserButtonShowsUserSheet() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        let addButton = app.navigationBars["Users"].buttons["Add"]
+        addButton.tap()
+        app.toolbars.buttons["Cancel"].tap()
+        addButton.tap()
+        // We can cancel a user add
+        
+        let emailTextField = app.textFields["Email"]
+        emailTextField.tap()
+        emailTextField.typeText("john@john.com")
+        app.keyboards.buttons["Return"].tap()
+        
+        let password1TextField = app.textFields["Password1"]
+        password1TextField.tap()
+        password1TextField.typeText("asdfasdf") // not a valid password
+        app.keyboards.buttons["Return"].tap()
+
+        //check that done is not enabled
+        XCTAssertEqual(app.buttons["Done"].enabled, false)
+
+        password1TextField.tap()
+        password1TextField.typeText("asdfasdf123")
+        app.childrenMatchingType(.Window).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).tap()
+        //check that done is enabled
+        XCTAssertEqual(app.buttons["Done"].enabled, true)
+
+        emailTextField.tap()
+        emailTextField.typeText("john") //not a valid email
+        app.childrenMatchingType(.Window).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).tap()
+        //check that done button is disabled
+        XCTAssertEqual(app.buttons["Done"].enabled, false)
+
+        emailTextField.tap()
+        emailTextField.typeText("john@john.com") //not a valid email
+        app.childrenMatchingType(.Window).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).childrenMatchingType(.Unknown).elementAtIndex(0).tap()
+
+        //click done button
+        XCUIApplication().toolbars.buttons["Done"].tap()
+
+        //check that john@john.com is now in master list
+
     }
     
 }
